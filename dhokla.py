404fb7002d5ccb2dce240cf3d4b687731674501c
amssGuard <97592173+amssGuard@users.noreply.github.com>
2024-10-26 23:47:23 +0530

    error

--- dhokla.py
+++ dhokla.py
@@ -1,4 +1,9 @@
 ####################################
+#IMPORTS
+####################################
+from string_with_arrow import *
+
+####################################
 #Constant
 ####################################
 DIGITS = "1234567890"
@@ -13,7 +18,7 @@ class Position:
         self.col = col
         self.text = text
 
-    def advance(self,currCharacter):
+    def advance(self,currCharacter=None):
         self.idx +=1
         self.col +=1
 
@@ -38,30 +43,44 @@ class Error:
 
     def as_string(self):
         result = f'{self.errorName}:{self.detail}'
+        result += '\n\n'+ string_with_arrows(self.posStart.text,self.posStart,self.posEnd)
         return result
 
 class IllegalCharacterError(Error):
     def __init__(self,posStart,posEnd,detail) -> None:
         super().__init__(posStart,posEnd,"Illegal Character",detail)
+
+class IllegalSyntaxError(Error):
+    def __init__(self,posStart,posEnd,detail) -> None:
+        super().__init__(posStart,posEnd,"Illegal Syntax",detail)
         
 
 
 ####################################
 #TOKENS
 ####################################
-TT_INT = 'INT'
-TT_FLOAT = 'FLOAT'
-TT_PLUS = 'PLUS'
-TT_MINUS = 'MINUS'
-TT_MUL = 'MUL'
-TT_DIV = 'DIV'
+TT_INT    = 'INT'
+TT_FLOAT  = 'FLOAT'
+TT_PLUS   = 'PLUS'
+TT_MINUS  = 'MINUS'
+TT_MUL    = 'MUL'
+TT_DIV    = 'DIV'
 TT_LPAREN = 'LPAREN'
 TT_RPAREN = 'RPAREN'
+TT_EOF    = 'EOF'
 
 class Token:
-    def __init__(self,type_,value=None) -> None:
+    def __init__(self,type_,value=None,posStart=None,posEnd=None) -> None:
         self.type = type_
         self.value = value
+
+        if posStart: 
+            self.posStart = posStart.copy()
+            self.posEnd = posStart.copy()
+            self.posEnd.advance()
+        
+        if posEnd:
+            self.posEnd = posEnd
 
     def __repr__(self) -> str:
         if self.value: return f'{self.type}:{self.value}'
@@ -91,16 +110,16 @@ class Lexer:
                 self.advance()
                 continue
             if self.currChar == '+':
-                tokens.append(Token(TT_PLUS))
+                tokens.append(Token(TT_PLUS,posStart=self.position))
                 self.advance()
             elif self.currChar == '-':
-                tokens.append(Token(TT_MINUS))
+                tokens.append(Token(TT_MINUS,posStart=self.position))
                 self.advance()
             elif self.currChar == '*':
-                tokens.append(Token(TT_MUL))
+                tokens.append(Token(TT_MUL,posStart=self.position))
                 self.advance()
             elif self.currChar == '/':
-                tokens.append(Token(TT_DIV))
+                tokens.append(Token(TT_DIV,posStart=self.position))
                 self.advance()
             elif self.currChar in DIGITS:
                 tokens.append(self.make_number())
@@ -109,11 +128,13 @@ class Lexer:
                 char = self.currChar
                 self.advance()
                 return [],IllegalCharacterError(posStart,self.position,char)
-        return tokens
+        tokens.append(Token(TT_EOF,posStart=self.position))
+        return tokens,None
 
     def make_number(self):
         numstr = ''
         dot_cnt = 0
+        posStart = self.position.copy()
         while self.currChar != None and self.currChar in DIGITS + '.':
             if self.currChar == '.':
                 if dot_cnt == 1:
@@ -124,10 +145,34 @@ class Lexer:
                 numstr+=self.currChar
             self.advance()
         if dot_cnt==1:
-            return Token(TT_FLOAT,float(numstr))
+            return Token(TT_FLOAT,float(numstr),posStart,self.position)
         else:
-            return Token(TT_INT,int(numstr))
+            return Token(TT_INT,int(numstr),posStart,self.position)
 #####################################
+
+
+#####################################
+#PARSE RESULT
+#####################################
+
+class ParseResult:
+    def __init__(self) -> None:
+        self.error = None
+        self.node = None
+
+    def register(self,res):
+        if isinstance(res,ParseResult):
+            if res.error: self.error = res.error
+            return res.node
+        return res
+
+    def success(self,node):
+        self.node = node
+        return self
+    
+    def failure(self,error):
+        self.error = error
+        return self
 
 
 #####################################
@@ -164,12 +209,14 @@ class Parser:
             self.currentTok = self.token[self.idx]
         return self.currentTok
     
-    def factor(self): 
+    def factor(self):
+        res = ParseResult()
         tok = self.currentTok
 
         if tok.type in (TT_INT,TT_FLOAT):
-            self.advance()
-            return NumberNode(tok)
+            res.register(self.advance())
+            return res.success(NumberNode(tok))
+        return res.failure(IllegalSyntaxError(tok.posStart,tok.posEnd,"Expected an Integer or Float"))
 
     def term(self):
         return self.binOp(self.factor,(TT_MUL,TT_DIV))
@@ -178,18 +225,23 @@ class Parser:
         return self.binOp(self.term,(TT_PLUS,TT_MINUS))
 
     def binOp(self,func,ops):
-        left = func()
+        res = ParseResult()
+        left = res.register(func())
+        if res.error: return res
         while self.currentTok is not None and self.currentTok.type in ops:
             op_tok = self.currentTok
-            self.advance()
-            right = func()
+            res.register(self.advance())
+            right = res.register(func())
+            if res.error: return res
             left = BinOpNode(left,right,op_tok)
-        return left
+        return res.success(left)
 
         
 
     def parse(self):
         res = self.expr()
+        if not res.error and self.currentTok.type != TT_EOF:
+            return res.failure(IllegalSyntaxError(self.currentTok.posStart,self.currentTok.posEnd,"expected '+','-','*'or'/'"))
         return res
     
 #####################################
@@ -201,5 +253,5 @@ def run(text):
     parse = Parser(token)
     ast = parse.parse()
 
-    return token,ast
+    return ast.node,ast.error#token,ast.node
     
